84ed29f599242a3a47fbd77de449cd0e74760914
Nour <n-4i@hotmail.com>
2021-02-21 15:17:26 +0300

    XVoice view

---
+++ resources/views/ControlPanel/Xvoice/read.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.app")
+@section("content")
+        <div class="row">
+            <div class="col-md-6 -pull-left float-left">
+                <label style="size: 25px;">Title</label>
+            </div>
+            <div class="col-md-6 float-right">
+                <label>2021-2-2</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p>asdasfwajsgldjysafgdkhdgaosy aspdaslduastda7 y</p>
+            </div>
+        </div>
+@endsection
